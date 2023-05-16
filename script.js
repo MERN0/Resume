33de05426eda3c56f88fe78143983a363d32319b
MERN0 <p.anshuman.2002@gmail.com>
2023-05-16 22:10:20 +0530

    animation home
    added multiple line animations to home page.

--- script.js
+++ script.js
@@ -48,3 +48,13 @@ ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
 ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', {origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'})
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'})
+
+
+// typed js 
+const typed = new Typed('.multiple-text', {
+  strings: ['Coder.', 'Frontend Developer.', 'Web Develoeper.'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
